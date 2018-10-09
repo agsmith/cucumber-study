fe85628ce4282d86b147062cf61474a1703742a7
Adam Smith <adam_smith3@comcast.com>
2018-10-09 09:05:09 -0400

    Initial push

---
+++ src/test/java/cuketest/RunCukeTest.java
@@ -0,0 +1,11 @@
+package cuketest;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCukeTest {
+}
